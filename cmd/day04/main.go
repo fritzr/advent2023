ce3887941724b20025488fd473c81f80817460d7
Fritz Reese <fritzoreese@gmail.com>
2023-12-04 00:33:32 -0500

    Initial success at day 4 part 2

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -17,6 +17,18 @@ func parseNumbers(numberFields string) []int {
   return result
 }
 
+func countWins(winners []int, present []int) int {
+  count := 0
+  for _, winner := range winners {
+    for _, have := range present {
+      if winner == have {
+        count += 1
+      }
+    }
+  }
+  return count
+}
+
 func main() {
 	lines, err := util.ReadInputLines(4)
 	if err != nil {
@@ -24,26 +36,27 @@ func main() {
 	}
 
   score := 0
-  for _, line := range lines {
+  totalCopies := int64(0)
+  gameCopies := make([]int64, len(lines))
+  for gameIndex, line := range lines {
     _, numberSets, _ := strings.Cut(line, ": ")
     winningStr, presentStr, _ := strings.Cut(numberSets, "|")
     winning := parseNumbers(winningStr)
     present := parseNumbers(presentStr)
-
-    count := 0
-    for _, winner := range winning {
-      for _, have := range present {
-        if winner == have {
-          count += 1
-        }
-      }
+    wins := countWins(winning, present)
+    if wins > 0 {
+      score += 1 << (wins - 1)
     }
-    if count > 0 {
-      score += 1 << (count - 1)
+    thisCopies := gameCopies[gameIndex] + 1
+    totalCopies += thisCopies
+    for copyIndex := gameIndex + 1; copyIndex < len(gameCopies) && copyIndex <= gameIndex + wins; copyIndex++ {
+      // fmt.Printf("won %d copies of %d\n", thisCopies, copyIndex + 1)
+      gameCopies[copyIndex] += thisCopies
     }
   }
 
 	fmt.Println(score)
+  fmt.Println(totalCopies)
 }
 
 // vim: set ts=2 sw=2:
