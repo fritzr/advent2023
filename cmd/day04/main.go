0a8c24495bf7afc5191f24be47c2fbf59b641e07
Fritz Reese <fritzoreese@gmail.com>
2023-12-08 12:41:59 -0500

    go fmt day 4

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"advent2023/util"
-  "fmt"
-  "strings"
-  "strconv"
-  "log"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 )
 
 type Set[T comparable] map[T]bool
 
 func parseNumbers(numberFields string, f func(int)) {
-  for _, field := range strings.Fields(numberFields) {
-    value, _ := strconv.Atoi(field)
-    f(value)
-  }
+	for _, field := range strings.Fields(numberFields) {
+		value, _ := strconv.Atoi(field)
+		f(value)
+	}
 }
 
 func parseNumberList(numberFields string) []int {
-  result := make([]int, 0)
-  parseNumbers(numberFields, func(value int) { result = append(result, value) })
-  return result
+	result := make([]int, 0)
+	parseNumbers(numberFields, func(value int) { result = append(result, value) })
+	return result
 }
 
 func parseNumberSet(numberFields string) Set[int] {
-  result := make(Set[int])
-  parseNumbers(numberFields, func(value int) { result[value] = true })
-  return result
+	result := make(Set[int])
+	parseNumbers(numberFields, func(value int) { result[value] = true })
+	return result
 }
 
 func countWins(winners Set[int], numbers []int) int {
-  count := 0
-  for _, number := range numbers {
-    if winners[number] {
-      count += 1
-    }
-  }
-  return count
+	count := 0
+	for _, number := range numbers {
+		if winners[number] {
+			count += 1
+		}
+	}
+	return count
 }
 
 func main() {
@@ -45,28 +45,28 @@ func main() {
 		log.Fatalf("%s", err)
 	}
 
-  score := 0
-  totalCopies := int64(0)
-  gameCopies := make([]int64, len(lines))
-  for gameIndex, line := range lines {
-    _, numberSets, _ := strings.Cut(line, ": ")
-    winningStr, presentStr, _ := strings.Cut(numberSets, "|")
-    winning := parseNumberSet(winningStr)
-    present := parseNumberList(presentStr)
-    wins := countWins(winning, present)
-    if wins > 0 {
-      score += 1 << (wins - 1)
-    }
-    thisCopies := gameCopies[gameIndex] + 1
-    totalCopies += thisCopies
-    for copyIndex := gameIndex + 1; copyIndex < len(gameCopies) && copyIndex <= gameIndex + wins; copyIndex++ {
-      // fmt.Printf("after game %d, total = %d, copies = %v\n", gameIndex + 1, totalCopies, gameCopies[gameIndex:])
-      gameCopies[copyIndex] += thisCopies
-    }
-  }
+	score := 0
+	totalCopies := int64(0)
+	gameCopies := make([]int64, len(lines))
+	for gameIndex, line := range lines {
+		_, numberSets, _ := strings.Cut(line, ": ")
+		winningStr, presentStr, _ := strings.Cut(numberSets, "|")
+		winning := parseNumberSet(winningStr)
+		present := parseNumberList(presentStr)
+		wins := countWins(winning, present)
+		if wins > 0 {
+			score += 1 << (wins - 1)
+		}
+		thisCopies := gameCopies[gameIndex] + 1
+		totalCopies += thisCopies
+		for copyIndex := gameIndex + 1; copyIndex < len(gameCopies) && copyIndex <= gameIndex+wins; copyIndex++ {
+			// fmt.Printf("after game %d, total = %d, copies = %v\n", gameIndex + 1, totalCopies, gameCopies[gameIndex:])
+			gameCopies[copyIndex] += thisCopies
+		}
+	}
 
 	fmt.Println(score)
-  fmt.Println(totalCopies)
+	fmt.Println(totalCopies)
 }
 
 // vim: set ts=2 sw=2:
